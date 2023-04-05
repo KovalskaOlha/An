3abceaeb3ff7689e2634afacc5022351e34934ce
KovalskaOlha <unlimolg@gmail.com>
2023-04-04 22:31:47 -0400

    olhas commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         Animal an=new Animal();
         an.animal="Dog";
         an.breed="Pitbull";
+
+        Cat c=new Cat();
+        c.breed="American shorthair";
+        c.name="tango";
     }
 }
